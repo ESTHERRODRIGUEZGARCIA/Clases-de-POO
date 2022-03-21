9704168d31cea34c63daf350e89bcb8e7e07cc07
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-22 00:47:17 +0100

    más o menos entendido. Continúo luego

--- Clases/puzzle.py
+++ Clases/puzzle.py
@@ -17,3 +17,5 @@ print(A.y(aa, (a,z)))
 print(aa.y((z,1,'z'))) 
 
 # el primer valor será el cero por estar la tupla t vacía
+# el segundo valor es el 1 por estar en la casilla 2
+#luego, tendríamos el valor z que sería el 3
