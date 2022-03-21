6c5d994be53245bc6d9f9c713e7c5c460533e4e4
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-22 00:28:18 +0100

    lo tenía puesto dos veces

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from Clases.Palindromo import *
 def ejercicios():
     variable = int(input("Seleccione que ejercicio desea ejecutar: \n1. Palíndromo - método de clase\n2. Palíndromo - método de instancias\n3. Puzzle \n4. Logger\n"))
     if variable == 1:
-        arg = input("¿Qué argumento quieres comprobar? \n")
+        arg = input( )
         Palindromo.esPalindromo(arg)
     elif variable == 2:
         from Clases import instancia
