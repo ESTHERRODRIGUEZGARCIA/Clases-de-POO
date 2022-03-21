8149e08a9eed0dfe0d600678ec290951e08c4ee1
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-22 00:31:59 +0100

    comienzo a descifrar ejer3

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from Clases.Palindromo import *
 def ejercicios():
     variable = int(input("Seleccione que ejercicio desea ejecutar: \n1. Palíndromo - método de clase\n2. Palíndromo - método de instancias\n3. Puzzle \n4. Logger\n"))
     if variable == 1:
-        arg = input( )
+        arg = input( "Introduzca la palabra que desee saber si es un palíndromo: " )
         Palindromo.esPalindromo(arg)
     elif variable == 2:
         from Clases import instancia
