2fad74326a828525c753b42254a97000cf5f38d9
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-22 00:00:29 +0100

    se tiene que llamar igual

---
+++ Clases/Palindromo.py
@@ -0,0 +1,22 @@
+# ejercicio: comprobar si una cadena de caracteres es palíndromo
+
+class Palindromo():
+    def esPalindromo(arg):
+        arg = input("Introduzca la palabra que desee saber si es un palíndromo. " )
+        arg = arg.lower()
+        arg = arg.replace(" ", "")
+        if str(arg) == "".join(reversed(arg)):
+            print(True)
+        else:
+            print(False)
+        
+        longitud = len(arg)
+        if longitud < 1:
+            print(True)
+        else:
+            if arg[0] == arg[-1]:
+                Palindromo.esPalindromo(arg[::-1])
+                print("Es un palíndromo. ")
+            else:
+                print("El argumento introducido no es un palíndromo. ")
+        
