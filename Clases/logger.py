a990152dcf179ec3adb47e6bd638eee503f16ea5
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-22 00:54:41 +0100

    el ejercicio me pide un mensaje

--- Clases/logger.py
+++ Clases/logger.py
@@ -1,4 +1,5 @@
 class logger():
     #objetivo: escribir un mensaje dado como parámetro en un archivo cada vez que se llame al método log(mensaje).
     def llamada(metodo):
+        mensaje= str(input("¿Qué mensaje desea escribir?: "))
         
