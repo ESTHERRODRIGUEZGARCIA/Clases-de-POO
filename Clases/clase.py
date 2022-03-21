a7e30ed27fd30741cb4813ef9c5ffb12d7e6b485
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-21 17:45:10 +0100

    ahora estaría el pal

--- Clases/clase.py
+++ Clases/clase.py
@@ -1,11 +1,16 @@
 # ejercicio: comprobar si una cadena de caracteres es palíndromo
 
 class Palindromo():
-    word = input()
-    if str(word) == str(word)[::-1] :
-        print("Palíndromo")
-    else:
-        print("Not Palíndromo")
-
-    if longitud % 2 == 0:
+    def esPalindromo(arg):
+        arg = input("Introduce el argumento que desea saber si es un palíndromo: ")
+        if arg =
+        if str(arg) == str(arg)[::-1] :
+            print("Palíndromo")
+        else:
+            print("Not Palíndromo")
         
+        longitud = len(arg)
+        if longitud % 2 == 0:
+            Palindromo.esPalindromo(arg[1:-1])
+        else:
+            print("El argumento introducido no es un palíndromo. ")
