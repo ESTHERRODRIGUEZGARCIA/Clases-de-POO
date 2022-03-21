1134f6b223feb831257bb3b43c41564a243387e4
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-22 00:56:23 +0100

    quier que se abra el texto...

--- Clases/logger.py
+++ Clases/logger.py
@@ -2,4 +2,4 @@ class logger():
     #objetivo: escribir un mensaje dado como parámetro en un archivo cada vez que se llame al método log(mensaje).
     def llamada(metodo):
         mensaje= str(input("¿Qué mensaje desea escribir?: "))
-        
+        archivo = open(logger)
