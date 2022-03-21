d9b848b3560aa5bdedb772a1106a95d687314a88
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-21 17:30:50 +0100

    palíndromo

--- Clases/clase.py
+++ Clases/clase.py
@@ -1,1 +1,8 @@
 # ejercicio: comprobar si una cadena de caracteres es palíndromo
+
+class Palindromo():
+    word = input()
+    if str(word) == str(word)[::-1] :
+        print("Palíndromo")
+    else:
+        print("Not Palíndromo")
