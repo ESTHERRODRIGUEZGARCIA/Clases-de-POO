daf8ead71b30b01ccd092dde749f60dcf90abbce
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-21 23:12:39 +0100

    creando main ejercicio 1

--- main.py
+++ main.py
@@ -4,4 +4,7 @@ from Clases import Logger
 from Clases.Logger import *
 
 def ejercicios():
-    
+    variable = int(input("Seleccione que ejercicio desea ejecutar: \na. Palíndromo - método de clase\nb. Palíndromo - método de instancias\nc. Puzzle \nd. Logger\n"))
+    if variable == 1:
+        arg = input("¿Qué argumento quieres comprobar? \n")
+        Palindromo.esPalindromo(arg)
