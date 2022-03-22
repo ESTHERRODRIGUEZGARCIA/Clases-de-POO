50c3875a61865460efd9f5cd83fdb7d0d89262a5
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-22 09:53:42 +0100

    importo librerías

--- Clases/logger.py
+++ Clases/logger.py
@@ -1,7 +1,11 @@
+
+import logging
+
 class logger():
     #objetivo: escribir un mensaje dado como parámetro en un archivo cada vez que se llame al método log(mensaje).
     def llamada(metodo):
         mensaje= str(input("¿Qué mensaje desea escribir?: "))
         archivo = open(logger)
         print("---Start log---")
+        logging.getLogger(__metodo__)
         print("---End log---")
