403adcf517e9e1dd7a359126f3c0cffe6243fe31
tereesaalvarez <alvdeporteresa@gmail.com>
2022-03-22 13:19:23 +0100

    commit

--- Clases/instancia.py
+++ Clases/instancia.py
@@ -1,17 +1,18 @@
+class instancia:
+    def palindromo(tema):
+            a,b= 'áéíóúÁÉÍÓÚüÜÑñ', 'aeiouAEIOUnN'
+            tilde = str.maketrans(a,b)
+            tema = tema.lower()
+            tema = tema.replace(" ", "")
+            tema = tema.translate(tilde)
+            lista = list(tema)
+            resultado = list(reversed(tema))
 
-def palindromo():
-        a,b= 'áéíóúÁÉÍÓÚüÜÑñ', 'aeiouAEIOUnN'
-        tilde = str.maketrans(a,b)
-        tema = tema.lower()
-        tema = tema.replace(" ", "")
-        lista = list(tema)
-        resultado = list(reversed(tema))
+            if lista == resultado:
+                print("Es palindromo!")
+            else:
+                print("No es palindromo!")
 
-        if lista == resultado:
-            print("Es palindromo!")
-        else:
-            print("No es palindromo!")
-
-        print(tema.upper())
-tema= str(input("Introduce un numero, palabra o frase para comprobar si es palindromo:  "))
-palindromo(tema)
+            print(tema.upper())
+    tema= str(input("Introduce un numero, palabra o frase para comprobar si es palindromo:  "))
+    palindromo(tema)
