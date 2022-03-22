3f82b3fb0c432b2b7fd3b9739ea7b44a281f29a1
tereesaalvarez <alvdeporteresa@gmail.com>
2022-03-22 13:08:26 +0100

    commit

--- Clases/instancia.py
+++ Clases/instancia.py
@@ -1,3 +1,6 @@
 class instantcia():
     def palindromo():
         a,b= 'áéíóúÁÉÍÓÚüÜÑñ', 'aeiouAEIOUnN'
+        tilde = str.maketrans(a,b)
+        tema = tema.lower()
+        tema = tema.replace(" ", "")
