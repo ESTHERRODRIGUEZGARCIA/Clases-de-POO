1a4ddc340d142af18da92aa33a6bf335a7304a24
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-22 13:37:52 +0100

    ejercicio puzzle realizado

--- Clases/puzzle.py
+++ Clases/puzzle.py
@@ -10,12 +10,9 @@ y = a.z             # sigue formando parte de la clase A por la anterior condici
 print(y(a))         # me devuelve <class 'Clases.puzzle.A'> por el self definido en z
 aa = a()            # instancia
 print(aa is a())    # False pues la condición no es verdadera
-z = aa.y            #función
-print(z(()))            # tupla vacía, devuelve el valor 0
+z = aa.y            # función
+print(z(()))                # tupla vacía, devuelve el valor 0
 print(a().y((a,)))          # tupla que devuelve len(a). Es 1 por estar formada sólo por a
-print(A.y(aa, (a,z)))           # tupla que devuelve la longitud. Es 2 por estar formada tanto por a como por z
-print(aa.y((z,1,'z'))) 
+print(A.y(aa, (a,z)))       # tupla que devuelve la longitud. Es 2 por estar formada tanto por a como por z
+print(aa.y((z,1,'z')))      # tupla que devuelve len( z, 1, 'z') que son 3 elementos
 
-# el primer valor será el cero por estar la tupla t vacía
-# el segundo valor es el 1 por estar en la casilla 2
-#luego, tendríamos el valor z que sería el 3
