5a0b221d8f11459c152d02d25a6dc415e9b68aef
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-22 13:41:43 +0100

    acabado

--- Clases/puzzle.py
+++ Clases/puzzle.py
@@ -1,16 +1,16 @@
-class A:                # estructura principal 
-    def z(self):        # función
+class A:                    # estructura principal 
+    def z(self):            # función
         return self 
  
-    def y(self, t):     # función
+    def y(self, t):         # función
         return len(t) 
  
-a = A               # vuelve a la clase A
-y = a.z             # sigue formando parte de la clase A por la anterior condición
-print(y(a))         # me devuelve <class 'Clases.puzzle.A'> por el self definido en z
-aa = a()            # instancia
-print(aa is a())    # False pues la condición no es verdadera
-z = aa.y            # función
+a = A                       # vuelve a la clase A
+y = a.z                     # sigue formando parte de la clase A por la anterior condición
+print(y(a))                 # me devuelve <class 'Clases.puzzle.A'> por el self definido en z
+aa = a()                    # instancia
+print(aa is a())            # False pues la condición no es verdadera
+z = aa.y                    # función
 print(z(()))                # tupla vacía, devuelve el valor 0
 print(a().y((a,)))          # tupla que devuelve len(a). Es 1 por estar formada sólo por a
 print(A.y(aa, (a,z)))       # tupla que devuelve la longitud. Es 2 por estar formada tanto por a como por z
