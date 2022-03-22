3f30a59232a82cf0b0c44408bd442c5c6d2d2ce4
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-22 10:36:05 +0100

    ya se abre el fichero

--- Clases/logger.py
+++ Clases/logger.py
@@ -5,15 +5,16 @@ class logger():
     #objetivo: escribir un mensaje dado como parámetro en un archivo cada vez que se llame al método log(mensaje).
     def llamada():
         mensaje= str(input("¿Qué mensaje desea escribir?: "))
-        veces = str(input("¿Cuántas veces desea recibir el mensaje?: "))
+        veces = int(input("¿Cuántas veces desea recibir el mensaje?: "))
         archivo = open("logger.txt", "a") #add
         
 
-        archivo.write("---Start log---")
+        archivo.write("---Start log---\n")
         for i in range(veces):
             archivo.write(mensaje)
 
-        archivo.write("---End log---")
+
+        archivo.write("\n---End log---"+ str(veces))
         archivo.close()
 
     
