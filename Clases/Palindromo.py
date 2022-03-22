cc7aabc324d5404c6c66d73678e5bfa88364626f
tereesaalvarez <alvdeporteresa@gmail.com>
2022-03-22 14:00:25 +0100

    Merge branch 'main' of https://github.com/ESTHERRODRIGUEZGARCIA/Clases-de-POO

--- Clases/Palindromo.py
+++ Clases/Palindromo.py
@@ -2,17 +2,17 @@
 
 class Palindromo():
     def esPalindromo(arg):
-        arg = input()
-        arg = arg.lower() #texto en minúsculas
-        arg = arg.replace(" ", "") # elimina espacios
         n, m = 'áéíóúüñÁÉÍÓÚÜÑ', 'aeiouunAEIOUUN' 
         tilde = str.maketrans(n, m)
+        arg = arg.lower() #texto en minúsculas
+        arg = arg.replace(" ", "") # elimina espacios
         arg = arg.translate(tilde) #elimina tildes
+        
         if str(arg) == "".join(reversed(arg)):
             print(True)
             print(arg, " sí es un palíndromo. ")
         else:
             print(False)
             print(arg, " no es un palíndromo. ")
-        
+    arg = str(input("Introduce para ver si es palindromo: "))
         
